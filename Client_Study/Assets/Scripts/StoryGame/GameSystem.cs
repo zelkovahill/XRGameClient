96bde4f806d2594cdeb81c0f222e6641797e3566
zelkovahill <changwoomin426@gmail.com>
2024-05-02 10:12:32 +0900

    feat : GameSystem 변경 및 StoryModel 추가

--- Client_Study/Assets/Scripts/StoryGame/GameSystem.cs
+++ Client_Study/Assets/Scripts/StoryGame/GameSystem.cs
@@ -46,8 +46,8 @@ namespace STROYGAME
     {
         public static GameSystem Instance;      //Scene 내부에서만 존재
 
-        public float delay = 0.1f;              //각 글자가 나타나는 시간
-        private string currentText = "";        //표시된 텍스트
+        //public float delay = 0.1f;              //각 글자가 나타나는 시간
+        //private string currentText = "";        //표시된 텍스트
         public TMP_Text textComponent;          //TextMeshPro 컴포넌트
 
         private void Awake()
@@ -63,52 +63,162 @@ namespace STROYGAME
         }
 
 
-        public GAMESTATE state;
+        public GAMESTATE currentState;
+        public Stats stats;
+        public StoryModel[] storyModels;
+        public int currentStoryIndex = 0;
 
-        public StoryTableObject[] storyModels;
-        public StoryTableObject currentModels;
-
-        private void Start()
+        public void ChangeState(GAMESTATE temp)
         {
-            currentModels = FindStoryModel(1);
-            StartCoroutine(ShowText());
+            currentState = temp;
+
+            if(currentState == GAMESTATE.STORYSHOW)
+            {
+                StoryShow(currentStoryIndex);
+            }
         }
 
-        StoryTableObject FindStoryModel(int number)
+        public void StoryShow(int number)
         {
-            StoryTableObject tempStoryModels = null;
-            for (int i = 0; i < storyModels.Length; i++)
+            StoryModel tempStoryModels = FindStoryModel(number);
+
+            // StorySystem.Instance.currentStoryModel = tempstoryModels;
+            // StorySystem.Instance.CoShowText();
+        }
+        
+        public void ApplyChoice(StoryModel.Result result)   // 스토리 선택시 결과
+        {
+            switch(result.resultType)
             {
-                if (storyModels[i].storyNumber == number)
+                case StoryModel.Result.EResultType.ChangeHp:
+                    // GameUI.Instance.UpdateHpUI();    // 나중에 추가
+                    ChangeStats(result);
+                    break;
+
+                case StoryModel.Result.EResultType.GoToNextStory:
+                    currentStoryIndex = result.value;   // 다음 이동 스토리 번호를 받아와서 실행
+                    ChangeState(GAMESTATE.STORYSHOW);
+                    ChangeStats(result);
+                    break;
+
+                case StoryModel.Result.EResultType.GoToRandomStory:
+                    RandomStory();
+                    ChangeState(GAMESTATE.STORYSHOW);
+                    ChangeStats(result); 
+                    break;
+
+                default:
+                    Debug.LogError("UnKnown effect Type");
+                    break;
+                 
+            }
+
+        }
+
+        public void ChangeStats(StoryModel.Result result)   // 상태 변경 함수
+        {
+            // 기본 스텟
+            if (result.stats.hpPoint > 0) stats.hpPoint += result.stats.hpPoint;
+            if (result.stats.spPoint > 0) stats.spPoint += result.stats.spPoint;
+
+            // 현재 상태
+            if (result.stats.currentHpPoint > 0) stats.currentHpPoint += result.stats.currentHpPoint;
+            if (result.stats.currentSpPoint > 0) stats.currentSpPoint += result.stats.currentSpPoint;
+            if (result.stats.currentXpPoint > 0) stats.currentXpPoint += result.stats.currentXpPoint;
+
+            // 능력치 상태
+            if (result.stats.strength > 0) stats.strength += result.stats.strength;
+            if (result.stats.dexterity > 0) stats.dexterity += result.stats.dexterity;
+            if (result.stats.consitiution > 0) stats.consitiution += result.stats.consitiution;
+            if (result.stats.wisdom > 0) stats.wisdom += result.stats.wisdom;
+            if (result.stats.Intelligence > 0) stats.Intelligence += result.stats.Intelligence;
+            if (result.stats.charisma > 0) stats.charisma += result.stats.charisma;
+
+        }
+
+
+        StoryModel RandomStory()    // 랜덤 스토리 리턴함수
+        {
+            StoryModel tempStoryModels = null;
+
+            List<StoryModel> StoryModelList = new List<StoryModel>();
+
+            for(int index =0; index < storyModels.Length; index++)  // MAIN 스토리들만 가져와서 List에 넣는다.
+            {
+                if (storyModels[index].storyType == StoryModel.ESTORYTYPE.MAIN) 
                 {
-                    tempStoryModels = storyModels[i];
+                    StoryModelList.Add(storyModels[index]);
+                }
+            }
+
+            tempStoryModels = StoryModelList[Random.Range(0, StoryModelList.Count)];    // MAIN들만 있는 리스트에서 랜덤으로 스토리 진행
+            currentStoryIndex = tempStoryModels.storyNumber;
+            print("currentStoryIndex : " + currentStoryIndex);
+
+            return tempStoryModels;
+        }
+
+        StoryModel FindStoryModel(int number) // 스토리 번호로 찾아주는 함수
+        {
+            StoryModel tempStoryModels = null;
+
+            for(int index =0; index < storyModels.Length; index++)
+            {
+                if (storyModels[index].storyNumber == number)
+                {
+                    tempStoryModels = storyModels[index];
                     break;
                 }
             }
             return tempStoryModels;
         }
 
-        IEnumerator ShowText()
-        {
-            for (int i = 0; i <= currentModels.storyText.Length; i++)
-            {
-                currentText = currentModels.storyText.Substring(0, i);
-                textComponent.text = currentText;
-                yield return new WaitForSeconds(delay);
-            }
-            yield return new WaitForSeconds(delay);
-        }
-
-
-
 #if UNITY_EDITOR
         [ContextMenu("Reset Story Models")]
         public void ResetStoryModels()
         {
-            storyModels = Resources.LoadAll<StoryTableObject>("");
+            storyModels = Resources.LoadAll<StoryModel>("");
             //Resources 폴더 아래 모든 StoryModel 불러 오기
         }
 #endif
+
+
+        // public StoryTableObject currentModels;
+
+        //private void Start()
+        //{
+        //    currentModels = FindStoryModel(1);
+        //    StartCoroutine(ShowText());
+        //}
+
+        //StoryTableObject FindStoryModel(int number)
+        //{
+        //    StoryTableObject tempStoryModels = null;
+        //    for (int i = 0; i < storyModels.Length; i++)
+        //    {
+        //        if (storyModels[i].storyNumber == number)
+        //        {
+        //            tempStoryModels = storyModels[i];
+        //            break;
+        //        }
+        //    }
+        //    return tempStoryModels;
+        //}
+
+        //IEnumerator ShowText()
+        //{
+        //    for (int i = 0; i <= currentModels.storyText.Length; i++)
+        //    {
+        //        currentText = currentModels.storyText.Substring(0, i);
+        //        textComponent.text = currentText;
+        //        yield return new WaitForSeconds(delay);
+        //    }
+        //    yield return new WaitForSeconds(delay);
+        //}
+
+
+
+
     }
 
 }
---
+++ Client_Study/Assets/Scripts/StoryGame/StoryModel.cs
@@ -0,0 +1,78 @@
+using STORYGAME;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewStory",menuName ="ScriptableObjects/StoryModel")]
+public class StoryModel : ScriptableObject
+{
+
+    public int storyNumber;
+    public Texture2D MainImage;
+    public bool storyDne;
+
+    public enum ESTORYTYPE
+    {
+        MAIN,
+        SUB,
+        SERIAL
+    }
+
+    public ESTORYTYPE storyType;
+
+    [TextArea(10, 10)]  // 인스펙터 text 영역 설정
+    public string storyText;
+    public Option[] options;
+
+    [System.Serializable]
+    public class Option
+    {
+        public string optionText;
+        public string buttonValue;
+
+        public EventCheck eventCheck;
+    }
+
+    public class EventCheck
+    {
+        public int checkValue;
+        public enum EventType : int
+        {
+            NONE,
+            GoToBattle,
+            CheckSTR,
+            CheckDEX,
+            CheckCON,
+            CheckINT,
+            CheckWIS,
+            CheckCHA
+        }
+
+        public EventType type;
+
+        public Result[] successResult;  // 선택지에 대한 성공 값 반영
+        public Result[] failedResult;   // 선택지에 대한 실패 값 반영
+    }
+
+
+    [System.Serializable]
+    public class Result
+    {
+
+        public enum EResultType : int
+        {
+            ChangeHp,
+            ChangeSp,
+            AddExperience,
+            GoToShop,
+            GoToNextStory,
+            GoToRandomStory,
+            GoToEnding
+        }
+
+
+        public EResultType resultType;
+        public int value;
+        public Stats stats;
+    }
+}
